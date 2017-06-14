f04d218d228a14addaa341562e638aba45f17a01
Jezzica <jezzicamae@gmail.com>
2017-06-14 15:07:24 -0400

    slider-fixed, but other section not showing up

--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
-var  mn = $(".main-nav");
-    mns = "main-nav-scrolled";
-    hdr = $('header').height();
+$(function(){
+  
+  var  mn = $(".main-nav");
+      mns = "main-nav-scrolled";
+      hdr = $('header').height();
 
-$(window).scroll(function() {
-  if( $(this).scrollTop() > hdr ) {
-    mn.addClass(mns);
-  } else {
-    mn.removeClass(mns);
-  }
+  $(window).scroll(function() {
+    if( $(this).scrollTop() > hdr ) {
+      mn.addClass(mns);
+    } else {
+      mn.removeClass(mns);
+    }
+  });
+
+
+$('.slick-slider').slick({
+  dots:true
+});
+
 });
